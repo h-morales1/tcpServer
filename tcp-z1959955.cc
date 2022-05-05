39b92c089d43a602805cefe79875fb86d0e88a72
Holzelh <spenser.vale.com@gmail.com>
2022-05-05 15:41:57 -0500

    just needs to write file contents to buffer

--- tcp-z1959955.cc
+++ tcp-z1959955.cc
@@ -13,10 +13,13 @@
 using namespace std;
 
 
+string get_req_dir(string s);
+string get_req_filn(string s);
 void chomp(char *s);
 int main(int argc, char *argv[])
 {
     char buffer[257];
+    //char reply[257];
     int sock, newSock;
     socklen_t serverlen, clientlen;
     ssize_t received;
@@ -107,6 +110,7 @@ int main(int argc, char *argv[])
             if(client_req.compare("/") == 0)
             {
                //check directory for an index.html
+               string webrt_files;
                while((dire_argpath_dirent = readdir(dire_argpath)) != NULL) // look through dir
                {
                    string f_name = dire_argpath_dirent->d_name;
@@ -118,12 +122,15 @@ int main(int argc, char *argv[])
                    {
                        if(f_name[0] != '.') // omit files starting with .
                        {
-                           cout << f_name << " ";
+                           //cout << f_name << " ";
+                           webrt_files += (f_name + " ");
                        }
                    }
 
                }
-               cout << "\n";
+               //add webrt file list to buffer
+                strncpy(buffer, webrt_files.c_str(), sizeof(buffer)); // add file list to buffer
+                buffer[sizeof(buffer)-1] = '\0';
             }
             else
             {
@@ -134,30 +141,124 @@ int main(int argc, char *argv[])
 
                 size_t cl_req_size = temp_cl_rq.length();
                 cout << "SIZE: " << cl_req_size << '\n';
-                //size_t tp_rq_size = temp_cl_rq.length();
                 char ending = temp_cl_rq.back();
                 cout << "Ending: " << ending << "\n";
-                //char endingC = client_req.at(cl_req_size-2);
-                //size_t final_pos = temp_cl_rq.find_last_of("/");
                 if(ending == '/')
                 {
                     //searching for dir
-                    //cout << "/ index: " << final_pos << '\n';
-                    cout << "YOURE LOOKING FOR A DIR!" << '\n';
+
+                    //concatenate webroot and the path passed in by user
+                    string final_path = argv[2] + client_req;
+                    DIR* dire_fnpath;
+                    struct dirent *dire_fnpath_dirent;
+                    dire_fnpath = opendir(final_path.c_str());
+                    if(dire_fnpath == 0) // exit if path doesn't exist
+                    {
+                        perror(argv[2]);
+                        // write error to buffer for client
+                        //stpcpy(buffer, "")
+                        exit(EXIT_FAILURE);
+                    }
+                    //dir exists so find index.html
+                    string files_dir;
+                    bool found_index = false;
+                    while((dire_fnpath_dirent = readdir(dire_fnpath)) != NULL) // look through dir
+                    {
+                        string fil_name = dire_fnpath_dirent->d_name; // name of current file
+                        if(fil_name.compare("index.html") == 0)
+                        {
+                            //index.html is found in this dir
+                            found_index = true;
+                            break;
+                        }
+                        else
+                        {
+                            // list all files in dir
+                            cout << fil_name << '\n';
+                            if(fil_name[0] != '.')
+                            {
+                                files_dir += (fil_name + " ");
+                            }
+                        }
+                    }
+                    if(found_index)
+                    {
+                        //write contents of index to buffer
+                    }
+                    else
+                    {
+                        //we didn't find index.html so list files in dir
+                        strncpy(buffer, files_dir.c_str(), sizeof(buffer)); // add file list to buffer
+                        buffer[sizeof(buffer)-1] = '\0';
+                    }
+
                 }
                 else
                 {
+                    //looking for file in dir  /fileOne.html
+                    //split dir and filename from request
+                    string req_filename = get_req_filn(client_req);
+                    string req_dir = get_req_dir(client_req);
+                    req_dir = req_dir + "/";
+                    //concatenate users directory with webroot
+                    string final_req_dir = argv[2] + req_dir;
+
+                   //prep file search
+                    DIR* dire_freqpath;
+                    struct dirent *dire_freqpath_dirent;
+
+                    dire_freqpath = opendir(final_req_dir.c_str());
+                    if(dire_freqpath == 0) // exit if path doesn't exist
+                    {
+                        perror(argv[2]);
+                        // write error to buffer for client
+                        //stpcpy(buffer, "")
+                        exit(EXIT_FAILURE);
+                    }
+                    //dir does exist so we  can search for file
+                    bool req_f_found = false;
+                    while((dire_freqpath_dirent = readdir(dire_freqpath)) != NULL) // look through dir
+                    {
+                        string fdir_filname = dire_freqpath_dirent->d_name;
+                        if(fdir_filname.compare(req_filename) == 0)
+                        {
+                            //file we are searching for is found
+                            req_f_found = true;
+                            break;
+                        }
+                        else
+                        {
+                            //we didn't find the file
+                            req_f_found = false;
+                        }
+                    }
+                    if(req_f_found)
+                    {
+                        //we found it so write its contents to buffer
+                        string not_found = "We found the file";
+                        strncpy(buffer, not_found.c_str(), sizeof(buffer)); // add file list to buffer
+                        buffer[sizeof(buffer)-1] = '\0';
+                    }
+                    else
+                    {
+                        //we didn't find it so write an error to buffer
+                        string not_found = "Error: " + req_filename + " not found";
+                        strncpy(buffer, not_found.c_str(), sizeof(buffer)); // add file list to buffer
+                        buffer[sizeof(buffer)-1] = '\0';
+                    }
+                    cout << "FILE: " << req_filename << '\n';
+                    cout << "DIR: " << req_dir << '\n';
                     cout << "Looking for a file" << '\n';
                 }
-                //cout << "endingC: " << endingC << '\n';
 
             }
 
-            if((write(newSock, buffer, received)) == -1) // write to client
+            if((write(newSock, buffer, strlen(buffer))) == -1) // write to client n was changed from received to buffer size
             {
                 perror("write mismatch");
                 exit(3);
             }
+            sleep(3);
             close(newSock);
             exit(0);
         }
@@ -166,6 +267,21 @@ int main(int argc, char *argv[])
     close(sock);
 
     return 0;
+}
+
+string get_req_filn(string s) // get filename from request
+{
+    // /fileOne.html
+
+    size_t loc = s.find_last_of('/');
+    return s.substr(loc +1);
+}
+
+string get_req_dir(string s) // get dir from request
+{
+    // /dub/dub/fileOne.html OR /fileOne.html
+    size_t loc = s.find_last_of('/');
+    return s.substr(0, loc);
 }
 
 void chomp(char *s)
