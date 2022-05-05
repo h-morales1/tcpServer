41baa1adcc8a08da837c776a1116f8f0e55d5792
Holzelh <spenser.vale.com@gmail.com>
2022-05-05 13:12:53 -0500

    server properly searches and finds index in root

--- tcp-z1959955.cc
+++ tcp-z1959955.cc
@@ -6,10 +6,14 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <dirent.h>
 #include <cstring>
 #include <iostream>
+#include <string>
 using namespace std;
 
+
+void chomp(char *s);
 int main(int argc, char *argv[])
 {
     char buffer[257];
@@ -18,6 +22,29 @@ int main(int argc, char *argv[])
     ssize_t received;
     struct sockaddr_in echoserver; // structure address of server
     struct sockaddr_in echoclient; // structure address of client
+    DIR* dire_argpath;
+    struct dirent *dire_argpath_dirent;
+
+    //exit if not enough arguments
+    if(argc < 2)
+    {
+        perror("Usage: ./z1959955 port path");
+        exit(EXIT_FAILURE);
+    }
+
+    //open dir passed in
+    dire_argpath = opendir(argv[2]);
+    if(dire_argpath == 0) // exit if path doesn't exist
+    {
+        perror(argv[2]);
+        exit(EXIT_FAILURE);
+    }
+
+    /*while((dire_argpath_dirent = readdir(dire_argpath)) != NULL) // see whats in directory
+    {
+        cout << "Detected: " << dire_argpath_dirent->d_name << "\n";
+    }*/
+
     //create sock
     if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
@@ -57,13 +84,83 @@ int main(int argc, char *argv[])
         {
             //forking
             cerr << "Client connected: " << inet_ntoa(echoclient.sin_addr) << '\n';
-            if((received = read(newSock, buffer, 256)) == -1)
+            if((received = read(newSock, buffer, 256)) == -1) // read from client
             {
                 perror("read failed");
                 exit(2);
             }
 
-            if((write(newSock, buffer, received)) == -1)
+            buffer[received] = '\0';
+            chomp(buffer);
+
+            cout << "Client wrote: " << buffer << "\n";
+            //looking for file or directory
+            string client_req = string(buffer);
+            //trim off GET
+            cout << "Before trim: " << client_req << "\n";
+            size_t pos = client_req.find("GET ");
+            client_req.erase(pos, 4);
+            cout << "After trim: " << client_req << "\n";
+            cout << "SIZE: " << client_req.size() << "\n";
+
+            //no directory passed in, just use webroot: req = GET /
+            if(client_req.compare("/") == 0)
+            {
+               //check directory for an index.html
+               while((dire_argpath_dirent = readdir(dire_argpath)) != NULL) // look through dir
+               {
+                   string f_name = dire_argpath_dirent->d_name;
+                   if(f_name.compare("index.html") == 0)
+                   {
+                       //found index.html in directory
+                   }
+                   else
+                   {
+                       if(f_name[0] != '.') // omit files starting with .
+                       {
+                           cout << f_name << " ";
+                       }
+                   }
+
+               }
+               cout << "\n";
+            }
+            else
+            {
+                // its not just /, there is more, now detect if searching for file or dir
+
+                //is the last character in string a / <-- signifies dir
+                string temp_cl_rq = client_req;
+                char tmp[500];
+                strcpy(tmp, temp_cl_rq.c_str());
+                for(char *p = tmp + strlen(tmp)-1;*p == '\r' || *p == '\n';p--)
+                {
+                    cout << *p << '\n';
+                    *p = '\0';
+                }
+                temp_cl_rq = string(tmp);
+                size_t cl_req_size = temp_cl_rq.length();
+                cout << "SIZE: " << cl_req_size << '\n';
+                //size_t tp_rq_size = temp_cl_rq.length();
+                char ending = temp_cl_rq.back();
+                cout << "Ending: " << ending << "\n";
+                char endingC = client_req.at(cl_req_size-2);
+                size_t final_pos = temp_cl_rq.find_last_of("/");
+                if((final_pos != std::string::npos) && (final_pos == cl_req_size - 3))
+                {
+                    //searching for dir
+                    cout << "/ index: " << final_pos << '\n';
+                    cout << "YOURE LOOKING FOR A DIR!" << '\n';
+                }
+                else
+                {
+                    cout << "Looking for a file" << '\n';
+                }
+                cout << "endingC: " << endingC << '\n';
+
+            }
+
+            if((write(newSock, buffer, received)) == -1) // write to client
             {
                 perror("write mismatch");
                 exit(3);
@@ -76,4 +173,12 @@ int main(int argc, char *argv[])
     close(sock);
 
     return 0;
+}
+
+void chomp(char *s)
+{
+    for(char *p = s + strlen(s)-1; *p == '\r' || *p == '\n'; p--)
+    {
+        *p = '\0';
+    }
 }
